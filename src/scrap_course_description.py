d4b03f1bd9402066335887d153ca90c6e93549c5
hollowsunsets <isabelgiang@gmail.com>
2019-12-16 22:44:00 -0800

    Improved course description parsing

--- src/scrap_course_description.py
+++ src/scrap_course_description.py
@@ -3,18 +3,17 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from requests.utils import requote_uri
 from collections import namedtuple
-from handle_args import campus_name, course_name, quarter_name
 
+from handle_args import campus_name, course_name, quarter_name
+# from utils import split_elem
+
+import itertools
 import re
 import argparse
 import requests
 import sys
 
 Course = namedtuple('Course', 'sln section credits days time building instructor status description enrolled_students max_students')
-
-def parse_course(course_info):
-    course_parts = course_info.strip().partition('\n')
-    return ( course_parts[0].split(), " ".join(course_parts[2].split()) )
 
 parser = argparse.ArgumentParser()
 parser.add_argument('course', type=course_name)
@@ -47,17 +46,24 @@ with urlopen(time_schedule_link) as response:
         if course_link:
             course_info = t.find_next_sibling('table')
             while not course_info.has_attr('bgcolor') or course_info['bgcolor'] == '#d3d3d3':
-                course_text = course_info.get_text()
-                # print(repr(course_text))
-                course_parts = course_text.strip().partition('\n')
-                course_info_generator = course_info.strings # this gets text in each tag
-                attr = re.split('\s\s+', course_parts[0].strip())
-                print(attr)
+                course_parts = course_info.get_text().strip().partition('\n')
                 description = " ".join(course_parts[2].split())
+                # Split on multiple space characters
+                # (text strings from UW website contain \r, \n and whitespace)
+                attributes = re.split('\s\s+', course_parts[0].strip())
+                for attr in attributes:
+                    if not re.fullmatch(r'[a-zA-Z, ]+', attr) and ' ' in attr:
+                        pos = attributes.index(attr)
+                        elem1, elem2 = attr.split()
+                        attributes[pos:pos + 1] = (elem1, elem2)
                 print(description)
+                print(attributes)
+                print(len(attributes))
                # print(Course(*attr[0].split(" "), attr[1], attr[2], attr[3], attr[4], " ".join([attr[5], attr[6]]),
                 #             attr[7], attr[8], description, 0))
                 course_info = course_info.find_next_sibling('table')
             break
+
+
 
 
