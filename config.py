45094004041087ac43c82fc746c96608f872ed56
hollowsunsets <isabelgiang@gmail.com>
2020-01-13 17:51:26 -0800

    Setup Heroku

--- config.py
+++ config.py
@@ -6,3 +6,17 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
