9d631b88fef3d91315cd451844d6dffce7caf8fb
hollowsunsets <isabelgiang@gmail.com>
2020-01-13 18:20:53 -0800

    Add SQLAlchemy

--- src/app.py
+++ src/app.py
@@ -1,8 +1,15 @@
+import os
+
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 api = Api(app)
+ab = SQLAlchemy(app)
 
 class HelloWorld(Resource):
     def get(self):
@@ -11,6 +18,10 @@ class HelloWorld(Resource):
 
 api.add_resource(HelloWorld, '/')
 
+@app.route('/<name>')
+def hello_name(name):
+    return f'Hello {name}!'
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
 
