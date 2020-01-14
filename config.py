6d5f5edf38a63f6605b4039f42a893aa80e41c3b
hollowsunsets <isabelgiang@gmail.com>
2020-01-13 17:41:39 -0800

    Improve configuration management

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
