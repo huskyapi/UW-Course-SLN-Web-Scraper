4cdb5ff6c7022fdf4138cea2dea17e218859cb5a
Isabel Giang <isabelgiang@gmail.com>
2020-12-22 19:59:10 -0800

    Remove poetry and change to pip

--- scraper/course_sln.py
+++ scraper/course_sln.py
@@ -2,9 +2,9 @@ BASE_URL = "https://sdb.admin.uw.edu/timeschd/uwnetid/sln.asp?"
 
 
 class CourseSLN:
-    def __init__(self, sln, quarter, year):
+    def __init__(self, quarter, year, sln):
         self.sln = sln
-        self.quarter = quarter
+        self.quarter = f"{quarter[:3].upper()}"
         self.year = year
 
         self.url = f"{BASE_URL}QTRYR={self.quarter}+{self.year}&SLN={self.sln}"
--- settings.py
+++
@@ -1,4 +0,0 @@
-import os
-
-PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
-BASE_DIR = os.path.dirname(PROJECT_DIR)
